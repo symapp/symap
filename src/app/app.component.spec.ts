e4984438c95b285252ef4df4db70945c47127ea9
Simao <simaodalmeida@hotmail.com>
2024-09-16 21:05:27 +0200

    setup jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        NoopAnimationsModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
   });
 
@@ -24,12 +29,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('symap');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, symap');
   });
 });
